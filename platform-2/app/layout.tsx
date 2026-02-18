2e9cde492c6cded483a1ddb056fe8af491563224
groverongo <ugartegrover@gmail.com>
2026-02-17 22:31:43 -0500

    feat: login page

--- platform-2/app/layout.tsx
+++ platform-2/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </NextThemesProvider>
       </body>
     </html>
   );
